2962d83dd7f5b005ffb7354f9c3bf30b00a108d4
legion <legion.rempel.dev@gmail.com>
2025-04-09 09:23:01 -0400

    Add image and link markdown extraction

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -34,6 +34,91 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
+    def test_split_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = TextNode.split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "code block")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_split_code_first(self):
+        node = TextNode("`This is the code block` this is text", TextType.TEXT)
+        new_nodes = TextNode.split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 2)
+        self.assertEqual(new_nodes[0].text, "This is the code block")
+        self.assertEqual(new_nodes[0].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[1].text, " this is text")
+        self.assertEqual(new_nodes[1].text_type, TextType.TEXT)
+
+    def test_split_two_code_blocks_first(self):
+        node = TextNode("`code block` text `code block`", TextType.TEXT)
+        new_nodes = TextNode.split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "code block")
+        self.assertEqual(new_nodes[0].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[1].text, " text ")
+        self.assertEqual(new_nodes[1].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[2].text, "code block")
+        self.assertEqual(new_nodes[2].text_type, TextType.CODE)
+
+    def test_split_bold(self):
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        new_nodes = TextNode.split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "bold")
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_split_italic(self):
+        node = TextNode("This is text with an _italic_ word", TextType.TEXT)
+        new_nodes = TextNode.split_nodes_delimiter(
+            [node], "_", TextType.ITALIC)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is text with an ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "italic")
+        self.assertEqual(new_nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_extract_markdown_images(self):
+        matches = TextNode.extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_two_images(self):
+        matches = TextNode.extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            matches,
+        )
+
+    def test_extract_markdown_links(self):
+        matches = TextNode.extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from leafnode import LeafNode
+import re
 
 
 class TextType(Enum):
@@ -18,9 +19,7 @@ class TextNode:
         self.url = url
 
     def __eq__(self, o):
-        return (
-            self.text == o.text and self.text_type == o.text_type and self.url == o.url
-        )
+        return self.text == o.text and self.text_type == o.text_type and self.url == o.url
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
@@ -34,10 +33,38 @@ class TextNode:
             case TextType.ITALIC:
                 return LeafNode("i", self.text)
             case TextType.CODE:
-                return LeafNode("```", self.text)
+                return LeafNode("`", self.text)
             case TextType.LINK:
                 return LeafNode("a", self.text, {"href": self.url})
             case TextType.IMAGE:
                 return LeafNode("img", "", {"src": self.url, "alt": self.text})
             case _:
                 raise Exception("invalid text type")
+
+    @staticmethod
+    def split_nodes_delimiter(old_nodes, delimiter, text_type):
+        new_nodes = []
+        for n in old_nodes:
+            if n.text_type != TextType.TEXT:
+                new_nodes.append(n)
+            else:
+                if n.text.count(delimiter) % 2 != 0:
+                    raise Exception(
+                        f"invalid markdown. no matching tag {delimiter}")
+                else:
+                    strings = n.text.split(delimiter)
+                    for idx, s in enumerate(strings):
+                        if len(s) > 0:
+                            tt = TextType.TEXT if idx % 2 == 0 else text_type
+                            new_nodes.append(TextNode(s, tt))
+        return new_nodes
+
+    @staticmethod
+    def extract_markdown_images(text):
+        matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+        return matches
+
+    @staticmethod
+    def extract_markdown_links(text):
+        matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+        return matches
