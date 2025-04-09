0f5729045ca554b156de4f4a5e7b1dd4fd2bb7fd
legion <legion.rempel.dev@gmail.com>
2025-04-09 14:10:14 -0400

    Add block_to_block_type

--- src/block.py
+++ src/block.py
@@ -21,3 +21,60 @@ class Block:
                 blocks.append(b)
 
         return blocks
+
+    @staticmethod
+    def block_to_block_type(block_text):
+        if Block.__is_heading(block_text):
+            return BlockType.HEADING
+        if Block.__is_code(block_text):
+            return BlockType.CODE
+        if Block.__is_quote(block_text):
+            return BlockType.QUOTE
+        if Block.__is_unordered_list(block_text):
+            return BlockType.UNORDERED_LIST
+        if Block.__is_ordered_list(block_text):
+            return BlockType.ORDERED_LIST
+        return BlockType.PARAGRAPH
+
+    @staticmethod
+    def __is_heading(block_text):
+        if (
+            block_text.startswith("# ")
+            or block_text.startswith("## ")
+            or block_text.startswith("### ")
+            or block_text.startswith("#### ")
+            or block_text.startswith("##### ")
+            or block_text.startswith("###### ")
+        ):
+            return True
+        return False
+
+    @staticmethod
+    def __is_code(block_text):
+        if block_text.startswith("```") and block_text.endswith("```"):
+            return True
+        return False
+
+    @staticmethod
+    def __is_quote(block_text):
+        lines = block_text.split("\n")
+        is_quote = True
+        for line in lines:
+            is_quote = is_quote and line.startswith(">")
+        return is_quote
+
+    @staticmethod
+    def __is_unordered_list(block_text):
+        lines = block_text.split("\n")
+        is_list = True
+        for line in lines:
+            is_list = is_list and line.startswith("- ")
+        return is_list
+
+    @staticmethod
+    def __is_ordered_list(block_text):
+        lines = block_text.split("\n")
+        is_list = True
+        for idx, line in enumerate(lines):
+            is_list = is_list and line.startswith(f"{idx + 1}. ")
+        return is_list
--- src/test_block.py
+++ src/test_block.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block import Block
+from block import Block, BlockType
 
 
 class TestBlock(unittest.TestCase):
@@ -22,6 +22,143 @@ class TestBlock(unittest.TestCase):
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
+        )
+
+    def test_is_heading(self):
+        self.assertEqual(BlockType.HEADING,
+                         Block.block_to_block_type("# Text"))
+        self.assertEqual(BlockType.HEADING,
+                         Block.block_to_block_type("## Text"))
+        self.assertEqual(BlockType.HEADING,
+                         Block.block_to_block_type("### Text"))
+        self.assertEqual(BlockType.HEADING,
+                         Block.block_to_block_type("#### Text"))
+        self.assertEqual(BlockType.HEADING,
+                         Block.block_to_block_type("##### Text"))
+        self.assertEqual(BlockType.HEADING,
+                         Block.block_to_block_type("###### Text"))
+
+    def test_is_not_heading(self):
+        self.assertNotEqual(BlockType.HEADING,
+                            Block.block_to_block_type("#Text"))
+        self.assertNotEqual(BlockType.HEADING,
+                            Block.block_to_block_type("####### Text"))
+        self.assertNotEqual(BlockType.HEADING,
+                            Block.block_to_block_type("Some Text"))
+
+    def test_is_code(self):
+        self.assertEqual(
+            BlockType.CODE, Block.block_to_block_type("```some code```"))
+        self.assertEqual(
+            BlockType.CODE, Block.block_to_block_type("``` some code ```"))
+        self.assertEqual(
+            BlockType.CODE,
+            Block.block_to_block_type("``` some code\nsome other code ```"),
+        )
+
+    def test_is_not_code(self):
+        self.assertNotEqual(
+            BlockType.CODE, Block.block_to_block_type("```some code"))
+        self.assertNotEqual(
+            BlockType.CODE, Block.block_to_block_type(" some code ```"))
+        self.assertNotEqual(
+            BlockType.CODE, Block.block_to_block_type(
+                "`` some code\nsome other code```")
+        )
+
+    def test_is_quote(self):
+        self.assertEqual(
+            BlockType.QUOTE, Block.block_to_block_type(">some quote"))
+        self.assertEqual(
+            BlockType.QUOTE, Block.block_to_block_type(
+                ">some quote\n>another quote")
+        )
+
+    def test_is_not_quote(self):
+        self.assertNotEqual(
+            BlockType.QUOTE, Block.block_to_block_type("ae>ke"))
+        self.assertNotEqual(
+            BlockType.QUOTE, Block.block_to_block_type(">ae>ke\nhelo"))
+
+    def test_is_unordered_list(self):
+        self.assertEqual(
+            BlockType.UNORDERED_LIST,
+            Block.block_to_block_type("- item 1\n- item 2\n- item 3"),
+        )
+        self.assertEqual(BlockType.UNORDERED_LIST,
+                         Block.block_to_block_type("- item 1"))
+
+    def test_is_not_unordered_list(self):
+        self.assertNotEqual(BlockType.UNORDERED_LIST,
+                            Block.block_to_block_type("item 1"))
+        self.assertNotEqual(
+            BlockType.UNORDERED_LIST, Block.block_to_block_type(
+                "- item 1\nitem 2")
+        )
+
+    def test_is_ordered_list(self):
+        self.assertEqual(BlockType.ORDERED_LIST,
+                         Block.block_to_block_type("1. first"))
+        self.assertEqual(
+            BlockType.ORDERED_LIST,
+            Block.block_to_block_type("1. first\n2. second\n3. third"),
+        )
+
+    def test_is_not_ordered_list(self):
+        self.assertNotEqual(
+            BlockType.ORDERED_LIST, Block.block_to_block_type(
+                " . first\n2. second")
+        )
+        self.assertNotEqual(
+            BlockType.ORDERED_LIST, Block.block_to_block_type(
+                "1. first\n. second")
+        )
+        self.assertNotEqual(
+            BlockType.ORDERED_LIST, Block.block_to_block_type(
+                "1. first\n . second")
+        )
+        self.assertNotEqual(
+            BlockType.ORDERED_LIST,
+            Block.block_to_block_type("1. first\n4. second\n3. third"),
+        )
+
+    def test_is_paragraph(self):
+        self.assertEqual(BlockType.PARAGRAPH,
+                         Block.block_to_block_type("any old text"))
+        self.assertEqual(
+            BlockType.PARAGRAPH, Block.block_to_block_type(
+                "any old text\nmore text")
+        )
+        self.assertEqual(BlockType.PARAGRAPH,
+                         Block.block_to_block_type("#stuff"))
+        self.assertEqual(BlockType.PARAGRAPH,
+                         Block.block_to_block_type("```stuff"))
+        self.assertEqual(
+            BlockType.PARAGRAPH, Block.block_to_block_type(
+                "stuff\n>more stuff")
+        )
+        self.assertEqual(
+            BlockType.PARAGRAPH, Block.block_to_block_type(
+                "stuff\n- more stuff")
+        )
+        self.assertEqual(
+            BlockType.PARAGRAPH, Block.block_to_block_type(
+                "1.stuff\n2. more stuff")
+        )
+
+    def test_is_not_paragraph(self):
+        self.assertNotEqual(BlockType.PARAGRAPH,
+                            Block.block_to_block_type("# Header"))
+        self.assertNotEqual(
+            BlockType.PARAGRAPH, Block.block_to_block_type("``` Code ```")
+        )
+        self.assertNotEqual(BlockType.PARAGRAPH,
+                            Block.block_to_block_type(">Quote"))
+        self.assertNotEqual(
+            BlockType.PARAGRAPH, Block.block_to_block_type("- Unordered Item")
+        )
+        self.assertNotEqual(
+            BlockType.PARAGRAPH, Block.block_to_block_type("1. First Item")
         )
 
 
