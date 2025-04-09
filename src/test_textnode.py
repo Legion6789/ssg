5edd17f1d7838c9c0937c5323c15c890cf18eab6
legion <legion.rempel.dev@gmail.com>
2025-04-08 20:03:43 -0400

    Add TextNode to HtmlNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -20,13 +20,19 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
     def test_url_none(self):
-        node = TextNode("This is not a link node", TextType.NORMAL)
+        node = TextNode("This is not a link node", TextType.TEXT)
         self.assertEqual(node.url, None)
 
     def test_url_not_none(self):
         node = TextNode("This is not a link node",
                         TextType.LINK, "https://google.com")
         self.assertEqual(node.url, "https://google.com")
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = node.to_html_node()
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
